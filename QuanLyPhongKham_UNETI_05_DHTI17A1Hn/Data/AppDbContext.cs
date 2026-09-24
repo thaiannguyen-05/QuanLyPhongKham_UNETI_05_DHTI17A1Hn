3c95eeb20fc462e7d5ee201a4fb1db39f6cec812
hieunguyen <nguyenhieu181005@gmail.com>
2026-09-24 11:35:40 +0700

    Add AppDbContext for Module 1

---
+++ QuanLyPhongKham_UNETI_05_DHTI17A1Hn/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyPhongKham_UNETI_05_DHTI17A1Hn.Models;
+namespace QuanLyPhongKham_UNETI05_TI17A1Hn.Data
+{
+    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaiKhoan> TaiKhoans { get; set; }
+
+        public DbSet<ChuyenKhoa> ChuyenKhoas { get; set; }
+
+        public DbSet<BacSi> BacSis { get; set; }
+
+        public DbSet<BenhNhan> BenhNhans { get; set; }
+
+        public DbSet<LichKham> LichKhams { get; set; }
+
+        public DbSet<PhieuDangKyKham> PhieuDangKyKhams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<TaiKhoan>()
+                .HasIndex(t => t.TenDangNhap)
+                .IsUnique();
+
+            modelBuilder.Entity<ChuyenKhoa>()
+                .HasIndex(c => c.TenChuyenKhoa)
+                .IsUnique();
+        }
+    }
+}
--- QuanLyPhongKham_UNETI_05_DHTI17A1Hn/Program.cs
+++ QuanLyPhongKham_UNETI_05_DHTI17A1Hn/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using QuanLyPhongKham_UNETI05_TI17A1Hn.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Cau hinh DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+// Cau hinh Session
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -9,21 +28,22 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
+// Phai co UseSession truoc Controller
+app.UseSession();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
